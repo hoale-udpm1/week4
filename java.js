16af008da908625427e59f7e738639e560fb3623
hoale-udpm1 <hoale08071999@gmail.com>
2020-08-24 17:05:18 +0700

    as'

--- java.js
+++
@@ -1,9 +0,0 @@
-// function square(number) {
-//     var result = number * number;
-//     return result;
-// }
-// function calculate() {
-//     var input = document.getElementById("input").value;
-//     var result=square(input);
-//     document.getElementById("result").innerHTML = result;
-// }
---
+++ monday/java.js
@@ -0,0 +1,21 @@
+// function square(number) {
+//     var result = number * number;
+//     return result;
+// }
+// function calculate() {
+//     var input = document.getElementById("input").value;
+//     var result=square(input);
+//     document.getElementById("result").innerHTML = result;
+// }
+
+    // function isPrime(num) {
+    // if(num < 2) return false;
+    // for (var i = 2; i < num; i++) {
+    // if(num%i==0)
+    // return false;
+    // }
+    // return true;
+    // }
+    // for(var i = 0; i < 100; i++){
+    // if(isPrime(i)) document.write(i + '<br>');
+    // }
